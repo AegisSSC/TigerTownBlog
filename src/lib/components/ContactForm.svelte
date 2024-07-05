91035a544bfb21fa0c25c625fc78b1b487ad5e16
Brandon Wingard <wingardbrandonm@gmail.com>
2024-07-05 00:10:52 -0400

    Adding in Components
    Adding in a Contact Form and Socials Bar component to be used.

---
+++ src/lib/components/ContactForm.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+
+</script>
+
+
+<!-- !TODO: This is where I want to build a contact form similar to the first copy of my portfolio page. -->
+
+<style>
+
+
+</style>
---
+++ src/lib/components/SocialsBar.svelte
@@ -0,0 +1,9 @@
+<script lang='ts'>
+
+</script>
+
+<!-- !TODO: This is where I want to have my social media stored as an array of icons that exist in boxes. Ideally this will live in the footer. -->
+
+<style>
+
+</style>
